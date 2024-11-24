94d88b29ab99abcc8c416b69259899f80e9c91ac
rat101 <rataonfire@proton.me>
2024-11-25 01:43:58 +0300

    set1/1 solved

---
+++ set1/1.py
@@ -0,0 +1,7 @@
+import codecs
+
+hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+flag = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+base64 = codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
+if base64.strip() == flag:
+    print(base64)
